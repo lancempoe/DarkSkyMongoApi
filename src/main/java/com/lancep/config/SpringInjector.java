c1c0afc70f9899cfd03792a859ab0f227ce14b57
Lance Poehler <lancepoehler@gmail.com>
2016-12-04 23:44:31 -0800

    cleaning up some files in spring

--- src/main/java/com/lancep/config/SpringInjector.java
+++ src/main/java/com/lancep/config/SpringInjector.java
@@ -1,7 +1,12 @@
 package com.lancep.config;
 
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 /**
  * If you have two different implementations of an interface
@@ -17,4 +22,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(value={"com.lancep.*"})
 public class SpringInjector {
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() throws Exception {
+        return new SimpleMongoDbFactory(new MongoClient(), MongoProperties.DARK_SKY_COLLECTION);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongoDbFactory());
+    }
 }
